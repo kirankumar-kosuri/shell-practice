3ff65c1c04bf288d46d196db2e1f85631a2f740b
kirankumar kosuri <kosurikirankumar7@gmail.com>
2025-11-12 12:27:19 +0530

    conversation

--- 03-variables.sh
+++ 03-variables.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 #Here no space before are after equal
-PERSON1=Kiran
-PERSON2=Praveen
+PERSON1=Pavan
+PERSON2=Kumar
 
 echo "$PERSON1:: Hi $PERSON2, How are you?"
 echo "$PERSON2:: Hello $PERSON1, I am fine. How are you?"
