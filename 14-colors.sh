58488aa9ba9a8f1c6b88eea4ba00cb081ca040fc
Kosuri Kirankumar <kosurikirankumar7@gmail.com>
2025-11-12 20:34:12 +0530

    colors

---
+++ 14-colors.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]; then
+    echo "ERROR:: PLEASE RUN THIS SCRIPT WITH ROOT PRIVELAGE"
+    exit 1
+fi
+
+validate(){
+    if [ $? -ne 0 ]; then
+        echo -e "Installing $2 ... $RED Failure ... $NORMAL"
+        exit 1
+    else    
+        echo -e "Installing $2 ... $YELLOW Success ... $NORMAL" 
+    fi
+}
+
+dnf list installed ansible 
+    if [ $? -ne 0 ]; then
+dnf install ansible -y
+validate $? "Ansible"
+    else
+    echo -e "Ansible is already Installed ... $YELLOW SKIPPING ... $NORMAL"
+    fi
+
+dnf list installed nginx
+    if [ $? -ne 0 ]; then
+dnf install nginx -y
+validate $? "Nginx"
+    else
+        echo -e "Nginx is already Installed ... $YELLOW SKIPPING ... $NORMAL"
+    fi
+dnf list installed python3
+    if [ $? -ne 0 ]; then
+dnf install python3 -y
+validate $? "Python3"
+    else
+         echo -e "Python3 is already Installed ... $YELLOW SKIPPING ... $NORMAL"
+    fi
