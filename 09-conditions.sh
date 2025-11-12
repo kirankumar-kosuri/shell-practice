c5e82b46ad6f1e7ca40f995b4d92db38092c5b64
kirankumar kosuri <kosurikirankumar7@gmail.com>
2025-11-12 16:11:09 +0530

    data-types

---
+++ 09-conditions.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Please enter the Number"
+read Number
+
+if [ $(($NUMBER % 2 ))]; then
+    echo "Given Number is $NUMBER is ODD"
+else
+    echo "Given Number is $NUMBER is EVEN"
+fi
+
