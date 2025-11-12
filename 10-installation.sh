a037081ead689f8423e6f866c9b45c5b6eb6a78a
Kosuri Kirankumar <kosurikirankumar7@gmail.com>
2025-11-12 16:38:23 +0530

    data-types

--- 10-installation.sh
+++ 10-installation.sh
@@ -5,3 +5,10 @@ USERID=$(id -u)
 if [ $USERID -ne 0 ]; then
     echo "ERROR:: PLEASE RUN THIS SCRIPT WITH ROOT PRIVELAGE"
 fi
+
+dnf install ansible -y
+
+if [ $? -ne 0 ]; then
+    echo "ERROR:: Installing Ansible is Failure"
+else    
+    echo "Installing Ansible is Success" 
