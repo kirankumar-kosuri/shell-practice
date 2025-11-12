bc84a55b315040fbebae413320d08dc0f9fb71f0
kirankumar kosuri <kosurikirankumar7@gmail.com>
2025-11-12 15:49:36 +0530

    data-types

--- 08-conditions.sh
+++ 08-conditions.sh
@@ -4,6 +4,9 @@ NUMBER=$1
 
 if [ $NUMBER -lt 10 ]; then
     echo "Given Number is $NUMBER is less than 10"
+
+elif [ $NUMBER -eq 10 ]; then
+    echo "Given Number is $NUMBER is equal to 10"
 else
-    echo "Given Number is $NUMBER is greater than or equal to 10"
+    echo "Given Number is $NUMBER is greater than 10"
 fi
