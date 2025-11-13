bbe7e39b896dab758ac7ffc3947d294a24357d49
Kosuri Kirankumar <kosurikirankumar7@gmail.com>
2025-11-13 12:28:26 +0530

    installation script

--- 14-colors.sh
+++ 14-colors.sh
@@ -1,39 +1,47 @@
 #!/bin/bash
+
 USERID=$(id -u)
 
+RED="\e[31m"
+GREEN="\e[32m"
+YELLOW="\e[33m"
+NORMAL="\e[0m"
+
+
 if [ $USERID -ne 0 ]; then
-    echo "ERROR:: PLEASE RUN THIS SCRIPT WITH ROOT PRIVELAGE"
+    echo "ERROR:: PLEASE RUN THIS SCRIPT UNDER ROOT ACCESS"
     exit 1
 fi
 
 validate(){
-    if [ $? -ne 0 ]; then
-        echo -e "Installing $2 ... $RED Failure ... $NORMAL"
-        exit 1
-    else    
-        echo -e "Installing $2 ... $YELLOW Success ... $NORMAL" 
-    fi
+    if [ $1 -ne 0 ]; then
+    echo "Installing $2 ... $RED FAILURE $NORMAL"
+    exit 1
+else
+    echo "Installing $2 ... $GREEN SUCCESS $NORMAL"
+fi
 }
 
-dnf list installed ansible 
+    dnf list installed ansible
     if [ $? -ne 0 ]; then
-dnf install ansible -y
-validate $? "ansible"
+    dnf install ansible -y
+    validate $? "Ansible"
     else
-    echo -e "Ansible is already Installed ... $YELLOW SKIPPING ... $NORMAL"
+        echo "Ansible is Already Installed ... $YELLOW SKIPPING $NORMAL"
     fi
 
-dnf list installed nginx
+    dnf list installed nginx
     if [ $? -ne 0 ]; then
-dnf install nginx -y
-validate $? "nginx"
-    else
-        echo -e "Nginx is already Installed ... $YELLOW SKIPPING ... $NORMAL"
+    dnf install nginx -y
+    validate $? "Nginx"
+    else 
+        echo "Nginx is Already Installed ... $YELLOW SKIPPING $NORMAL"
     fi
-dnf list installed python3
+    dnf list installed mysql
     if [ $? -ne 0 ]; then
-dnf install python3 -y
-validate $? "Python3"
+    dnf install mysql -y
+    validate $? "Mysql"
     else
-         echo -e "Python3 is already Installed ... $YELLOW SKIPPING ... $NORMAL"
+        echo "Mysql is Already Installed ... $YELLOW SKIPPING $NORMAL"
     fi
+
