1311deddbe7c906501856722c862538b3e1663d2
Kosuri Kirankumar <kosurikirankumar7@gmail.com>
2025-11-13 11:45:25 +0530

    installation script

--- 11-installation.sh
+++ 11-installation.sh
@@ -3,34 +3,33 @@
 USERID=$(id -u)
 
 if [ $USERID -ne 0 ]; then
-    echo "ERROR:: PLEASE RUN THIS SCRIPT WITH ROOT PRIVELAGE"
+    echo "ERROR:: PLEASE RUN THIS SCRIPT UNDER ROOT ACCESS"
     exit 1
 fi
-##############################################################################################################
-dnf install ansible -y
+
+    dnf install ansible -y
 
 if [ $? -ne 0 ]; then
-    echo "ERROR:: Installing Ansible is Failure"
+    echo "Installing Ansible is FAILURE"
     exit 1
-else    
-    echo "Installing Ansible is Success" 
+else
+    echo "Installing Ansible is SUCCESS"
 fi
-##############################################################################################################
-dnf install nginx -y
+
+    dnf install nginx -y
 
 if [ $? -ne 0 ]; then
-    echo "ERROR:: Installing Nginx is Failure"
+    echo "Installing  Nginx is FAILURE"
     exit 1
-else    
-    echo "Installing Nginx is Success" 
+else
+    echo "Installing Nginx is SUCCESS"
 fi
-##############################################################################################################
-dnf install python3 -y
+
+    dnf install botocore boto3 -y
 
 if [ $? -ne 0 ]; then
-    echo "ERROR:: Installing Python3 is Failure"
+    echo "Installing BotoCore Boto3 is FAILURE"
     exit 1
-else    
-    echo "Installing Python3 is Success" 
+else
+    echo "Installing BotoCore Boto3 is SUCCESS"
 fi
-##############################################################################################################
