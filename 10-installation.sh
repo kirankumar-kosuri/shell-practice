868cf00ba64b3e9ef369fb639970467139abcba5
Kosuri Kirankumar <kosurikirankumar7@gmail.com>
2025-11-13 11:28:04 +0530

    installation script

--- 10-installation.sh
+++ 10-installation.sh
@@ -4,11 +4,13 @@ USERID=$(id -u)
 
 if [ $USERID -ne 0 ]; then
     echo "ERROR:: PLEASE RUN THIS SCRIPT WITH ROOT PREVELAGE"
+    exit 1
 fi
 
     dnf install nginx -y
 if [ $? -ne 0 ]; then
     echo "Installing Nginx is FAILURE"
+    exit 1
 else
     echo "Installing Nginx is SUCCESS"
 fi
