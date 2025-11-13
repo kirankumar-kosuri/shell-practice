6f1558fe691f43502457df1f9e4b3bf40a8d14ad
Kosuri Kirankumar <kosurikirankumar7@gmail.com>
2025-11-13 11:33:22 +0530

    installation script

--- 10-installation.sh
+++ 10-installation.sh
@@ -4,12 +4,14 @@ USERID=$(id -u)
 
 if [ $USERID -ne 0 ]; then
     echo "ERROR:: PLEASE RUN THIS SCRIPT WITH ROOT ACCESS"
+    exit 1
 fi
 
     dnf install python3 - y
 
 if [ $? -ne 0 ]; then
     echo "Installing Python3 is FAILURE"
+    exit 1
 else
     echo "Installing Python3 is SUCCESS"
 fi
