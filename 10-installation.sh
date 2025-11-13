ad560a0fa731e2e5daa2393a5633e9ff1d75cbd9
Kosuri Kirankumar <kosurikirankumar7@gmail.com>
2025-11-13 11:26:53 +0530

    installation script

--- 10-installation.sh
+++ 10-installation.sh
@@ -1,26 +1,14 @@
-# #!/bin/bash
-
-# USERID=$(id -u)
-
-# if [ $USERID -ne 0 ]; then
-#     echo "ERROR:: PLEASE RUN THIS SCRIPT WITH ROOT PRIVELAGE"
-#     exit 1
-# fi
-
-# dnf install ansible -y
-
-# if [ $? -ne 0 ]; then
-#     echo "ERROR:: Installing Ansible is Failure"
-#     exit 1
-# else    
-#     echo "Installing Ansible is Success" 
-# fi
-
 #!/bin/bash
 
 USERID=$(id -u)
 
 if [ $USERID -ne 0 ]; then
     echo "ERROR:: PLEASE RUN THIS SCRIPT WITH ROOT PREVELAGE"
-    exit 1
+fi
+
+    dnf install nginx -y
+if [ $? -ne 0 ]; then
+    echo "Installing Nginx is FAILURE"
+else
+    echo "Installing Nginx is SUCCESS"
 fi
